778f541ff4ffcdc87bfbe4d99e149951a09a8ec1
Chris Simmons <chris@turing.edu>
2024-02-05 18:03:36 -0500

    Formats routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,11 +8,11 @@ Rails.application.routes.draw do
   get '/register', to: 'users#new', as: 'register_user'
 
   resources :users, only: [:show, :create] do
-      get "/discover", to: "discover#index"
-      post "/movies", to: "movies#results"
-      get "/movies/:movie_id", to: "movies#show"
-      get "/movies/:movie_id/viewing_party/new", to: "viewing_parties#new"
-      post "/movies/:movie_id/viewing_party", to: "viewing_parties#create"
+    get "/discover", to: "discover#index"
+    post "/movies", to: "movies#results"
+    get "/movies/:movie_id", to: "movies#show"
+    get "/movies/:movie_id/viewing_party/new", to: "viewing_parties#new"
+    post "/movies/:movie_id/viewing_party", to: "viewing_parties#create"
   end
 
 end
